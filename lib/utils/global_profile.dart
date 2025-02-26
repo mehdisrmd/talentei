4d0640b38556d0c10b689727a7d997ba1d8532bb
vJDMg437FZy8JSu <mehdi.sarmadi2019@gmail.com>
2025-02-26 12:06:15 +0330

    create global profile widget

---
+++ lib/utils/global_profile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GlobalProfile extends StatelessWidget {
+  const GlobalProfile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(3.5),
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFFE7E8F4), width: 1),
+        shape: BoxShape.circle,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Image.asset(
+          'assets/images/profile.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
